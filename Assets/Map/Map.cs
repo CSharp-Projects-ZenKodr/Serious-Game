f30c698139c3a7ed3c25693c7b7f23c473831225
ulysse jobard <ulysse.jobard@orange.fr>
2020-12-07 17:56:10 +0100

    Basic moving cubic player

---
+++ Assets/Map/Map.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject player;
+    public int height;
+    public int width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale =  new Vector3(width,height,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Map/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            position.y += 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            position.y -= 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            position.x -= 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            position.x += 1;
+        }
+
+        this.GetComponent<Transform>().position = position;
+
+    }
+}
