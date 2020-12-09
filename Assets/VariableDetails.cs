6772a79f11abd113e6152e5b9e026ab498d78d51
ulysse jobard <ulysse.jobard@orange.fr>
2020-12-09 15:48:53 +0100

    UI, work in progress

---
+++ Assets/VariableDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VariableItem : MonoBehaviour
+{
+    public Text name = null;
+    public Text value = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
