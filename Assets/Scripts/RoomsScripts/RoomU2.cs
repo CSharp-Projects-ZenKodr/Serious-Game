1106eed57c4f671abba2aa816359d822a5c74136
Guillaume Demarles <g.demarles@yahoo.fr>
2020-12-12 22:16:40 +0100

    Tried to implement level U2

--- Assets/Scripts/RoomsScripts/RoomU2.cs
+++ Assets/Scripts/RoomsScripts/RoomU2.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomU1 : Room
+public class RoomU2 : Room
 {
-    public Animator redButton;
+    public Animator blueButton;
+    public Animator lever;
     public MapObject doorBlocker;
     private bool buttonPressed = false;
+    private bool leverTriggered = false;
     private bool doorOpen = false;
     // Start is called before the first frame update
     new void Start()
     {
         base.Start();
-        index = 1;
+        index = 2;
     }
 
     // Update is called once per frame
     new void Update()
     {
-        
+
     }
 
     public override void Event(string objName, bool activate)
@@ -26,17 +28,18 @@ public class RoomU1 : Room
         if (activate)
         {
             doorBlocker.collisionEnabled = false;
-            redButton.SetBool("isPushed", true);
+            blueButton.SetBool("isPushed", true);
+            lever.SetBool("isTrigger", true);
             doorAnimator.SetBool("isOpen", true);
             print("Bravo !");
             buttonPressed = true;
+            leverTriggered = true;
             doorOpen = true;
         }
         else
         {
+            blueButton.SetBool("isPushed", false);
             buttonPressed = false;
-            redButton.SetBool("isPushed", false);
-
         }
 
 
@@ -45,16 +48,18 @@ public class RoomU1 : Room
     public override string GetRoomVariablesToDisplay()
     {
         string variables = "";
-        variables += "Red Button Pressed = " + convertBool(buttonPressed);
+        variables += "Blue Button Pressed = " + convertBool(buttonPressed);
+        variables += "\n";
+        variables += "Lever triggered = " + convertBool(leverTriggered);
         variables += "\n";
         variables += "Door is Open = " + convertBool(doorOpen);
-        
+
         return variables;
     }
 
     public override string GetRoomCodeToDisplay()
     {
-        string code = "<b>if</b>(Red Button Pressed = <color=green>True</color>)\n\t <b>then</b> Door is Open = <color=green>True</color>";
+        string code = "<b>if</b>(Blue Button Pressed = <color=green>True</color>) and (Lever Trigger = <color=green>True</color>) \n\t <b>then</b> Door is Open = <color=green>True</color>";
         return code;
     }
 }
