57d9f62aefaa56cc3574a69d368ed69488d3eeec
ulysse jobard <ulysse.jobard@orange.fr>
2020-12-12 12:39:44 +0100

    Room U1 is working ! and small UI adjustment

---
+++ Assets/Scripts/RoomObjects/RoomObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomObject : MonoBehaviour
+{
+    private MainLogic mainLogic = null;
+    public string objName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainLogic = GameObject.Find("LogicTreatment").GetComponent<MainLogic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            mainLogic.Event(this.objName,true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            mainLogic.Event(this.objName,false);
+        }
+    }
+    
+}
